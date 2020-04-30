7abd09dba4840d7812cff2f776028749f05eda5a
Nicolasb813 <nicolasb813@gmail.com>
2020-04-29 21:20:17 -0300

    BT5 Creating & Deleting Landscapes

---
+++ BattleTank_4_20/Source/BattleTank_4_20/BattleTank_4_20GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_4_20GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_4_20_API ABattleTank_4_20GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
